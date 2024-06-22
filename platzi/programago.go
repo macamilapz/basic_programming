fc58d47944de636064db827de3365d2a6fef9971
macamilapz <macamilapz@gmail.com>
2024-06-22 13:15:32 -0500

    contar en go

---
+++ platzi/programago.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	var j int = 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+	j += 1
+	fmt.Printf("%d\n", j)
+
+}
